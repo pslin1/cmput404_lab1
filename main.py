5890a7500146ad17ad5e452a15e5b027b223b08d
Shu-Jun Lin <pslin@cs.ualberta.ca>
2019-01-08 09:15:49 -0700

    Script modified to print raw code from github

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ print(requests.__version__)
 r = requests.get("http://www.google.com")
 
 print(r.status_code)
+
+s = requests.get("https://raw.githubusercontent.com/pslin1/cmput404_lab1/master/main.py")
+
+print(s.text)
